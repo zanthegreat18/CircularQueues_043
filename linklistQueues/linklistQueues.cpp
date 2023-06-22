0afa42e64939a2b5cff0376697f923f0c1e3da1a
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-06-22 11:11:10 +0700

    commit 16 jika front position > rear position, adalah iterasi dari  front hingga akhir array

--- linklistQueues/linklistQueues.cpp
+++ linklistQueues/linklistQueues.cpp
@@ -80,6 +80,12 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queues_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
 		
 			
 
