eabf5fc261c9c8d380b088d3756d67ac7784d902
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-06-22 11:01:17 +0700

    commit 11 front dan rear position

--- linklistQueues/linklistQueues.cpp
+++ linklistQueues/linklistQueues.cpp
@@ -59,5 +59,12 @@ public:
 				FRONT = FRONT + 1;
 		}
 	}
+
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
 			
 
